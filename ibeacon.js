36e66fa2731a2ce4a6e71d4b92ff4598a1267c7f
Steve Trease <stevetrease@gmail.com>
2015-01-20 07:53:30 +0000

    Added NODE_ENV=production and startup logic.

--- ibeacon.js
+++ ibeacon.js
@@ -1,4 +1,18 @@
-var config = require('./config.json');
+console.log("process.env.NODE_ENV:" + process.env.NODE_ENV);
+switch (process.env.NODE_ENV) {
+	case 'development':
+		console.log ("production mode");
+		var config = require('./config.json');
+		break;
+	case 'production':
+	default:	
+		console.log ("development mode");
+		var config = require('./config.json');
+}
+
+
+
+
 var http = require('http');
 var redis = require('redis')
    ,redisClient = redis.createClient(parseInt(config.redis.port,10), config.redis.host);
